8375395e335d31e5c7a56359555d7d0fbf3399f9
Cj-bc <cj.bc-sd@outlook.jp>
2024-12-03 01:05:14 +0900

    feat: Adds initial implementation of AnimationEventRelay

---
+++ Runtime/AnimationEventRelay.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>A relay for Animator's AnimationEvent</summary>
+[RequireComponent(typeof(Animator))]
+public class AnimationEventRelay : MonoBehaviour
+{
+    /// <summary>Corresponding animator</summary>
+    public Animator animator;
+
+    /// <summary>Association of AnimationEventID and list of callback functions registered for it.</summary>
+    /// <remark>Key represents the AnimationEventID attached to each AnimationEvent. Value is a list of registered callback functions.</remark>
+    private Dictionary<int, List<Action>> registeredCallbbacks = new();
+    /// <summary>Association of set of AnimationEvent and AnimationEventID</summary>
+    private Dictionary<(AnimationClip, TimeSpan), int> animatoinEventIdMap = new();
+    private bool isInitialized = false;
+
+    private System.Random randomizer = new();
+
+    public void OnEnable()
+    {
+        if (isInitialized)
+            return;
+
+        animator ??= GetComponent<Animator>();
+        if (animator is null)
+        {
+            Debug.LogWarning("AnimationEventRelay should be attached to the same gameObject as Animator");
+            enabled = false;
+            return;
+        }
+    }
+
+    /// <summary>Inject new AnimationEvent and call given callback</summary>
+    /// <param name="callback">A function to call on registered timing</param>
+    public void InjectAnimationEventListener(string clipName, TimeSpan time, Action callback)
+    {
+        if (getClip(clipName) is AnimationClip c)
+        {
+            Inject(c, time, callback);
+        }
+    }
+
+    /// <summary>Inject new AnimationEvent at end of the clip and call given callback</summary>
+    public void InjectAnimationEndedEventListener(string clipName, Action callback)
+    {
+        if (getClip(clipName) is AnimationClip c)
+        {
+            Inject(c, TimeSpan.FromSeconds(c.length), callback);
+        }
+    }
+
+    public void RemoveAnimationEndedEventListener(string clipName, Action callback)
+    {
+        if (getClip(clipName) is AnimationClip c)
+        {
+            RemoveAnimationEventListener(clipName, TimeSpan.FromSeconds(c.length), callback);
+        }
+    }
+
+    /// <summary>Unregister callback of given time.</summary>
+    public void RemoveAnimationEventListener(string clipName, TimeSpan time, Action callback)
+    {
+        if (getClip(clipName) is AnimationClip c
+            && animatoinEventIdMap.TryGetValue(new(c, time), out int id)
+            && registeredCallbbacks.GetValueOrDefault(id) is List<Action> callbacks)
+        {
+            callbacks.Remove(callback);
+        }
+    }
+
+    /// <summary>Register animation event at given <c cref="time">time</c>, and arrange it to call given callback</summary>
+    private void Inject(AnimationClip clip, TimeSpan time, Action callback)
+    {
+        if (animatoinEventIdMap.TryGetValue((clip, time), out int id)
+            && registeredCallbbacks.TryGetValue(id, out List<Action> callbacks))
+        {
+            callbacks.Add(callback);
+        } else
+        {
+            id = randomizer.Next();
+            // Stores callback function for internal use
+            registeredCallbbacks.Add(id, new() {callback});
+            animatoinEventIdMap.Add((clip, time), id);
+
+            // Adds AnimationEvent to the clip
+            AnimationEvent ev = new AnimationEvent();
+            ev.time = (float)time.TotalSeconds;
+            ev.functionName = "Receiver";
+            ev.intParameter = id;
+            clip.AddEvent(ev);
+        }
+    }
+
+    private AnimationClip? getClip(string clipName)
+        => animator.runtimeAnimatorController.animationClips
+        .Where(clip => clip.name == clipName)
+        .FirstOrDefault();
+
+    private void Receiver(int arg)
+    {
+        if (registeredCallbbacks.TryGetValue(arg, out List<Action> callbacks))
+        {
+            callbacks.ForEach(callback => callback());
+        }
+    }
+}
